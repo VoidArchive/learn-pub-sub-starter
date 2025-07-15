48ed5116df9b78626f0ae2aecb0692f05df66994
voidarchive <voidarchived@gmail.com>
2025-07-15 10:59:21 +0545

    exchanges publish

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,8 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -19,6 +21,16 @@ func main() {
 	}
 	defer conn.Close()
 	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("couldn't open channel to RabbitMQ: %v", err)
+	}
+
+	val := routing.PlayingState{
+		IsPaused: true,
+	}
+	pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, val)
 
 	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
---
+++ internal/pubsub/publish.go
@@ -0,0 +1,28 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	body, err := json.Marshal(val)
+	if err != nil {
+		log.Fatalf("couldn't marshal the val: %v", err)
+	}
+
+	return ch.PublishWithContext(
+		context.Background(),
+		exchange,
+		key,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        body,
+		},
+	)
+}
